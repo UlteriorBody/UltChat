d8d04d352689e09b9d907e82be4c240d0cf8e474
Jarris White <ub4mgs@yahoo.com>
2018-07-14 16:09:29 -0500

    Version 1.1: Added config and arg parsing

--- Client.py
+++ Client.py
@@ -10,6 +10,97 @@ from Cryptodome.Cipher import PKCS1_OAEP
 import ast
 import urllib.request
 import shutil
+import configparser
+import argparse
+
+def parse_config():
+	global publicKeyFile, port, do_encrypt, server_address, key_length, debug
+	publicKeyFile_default = 'public.pem'
+	port_default = 5006
+	do_encrypt_default = False
+	server_address_default = '127.0.0.1'
+	key_length_default = 16
+	debug_default = False
+	config_file_default = 'client.ini'
+	
+	config = configparser.ConfigParser()
+	config.read(config_file)
+	section = "Client"
+	try:
+		config_client = config[section]
+	except:
+		publicKeyFile = publicKeyFile_default
+		port = port_default
+		do_encrypt = do_encrypt_default
+		server_address = server_address_default
+		key_length = key_length_default
+		debug = debug_default
+		if config_file != config_file_default:
+			print("\nSomething wrong with config file ({}).\n".format(config_file))
+		return
+	
+	errorWith = []
+	
+	publicKeyFile = config_client.get('publickeyfile', publicKeyFile_default)
+	
+	try:
+		port = config_client.getint('port', port_default)
+	except:
+		errorWith.append('port')
+		port = port_default
+	
+	try:
+		do_encrypt = config_client.getboolean('encrypt', do_encrypt_default)
+	except:
+		errorWith.append('encrypt')
+		do_encrypt = do_encrypt_default
+	
+	server_address = config_client.get('serveraddress', server_address_default)
+		
+	valid_key_lengths = [16, 24, 32]
+	try:
+		key_length = config_client.getint('keylength', key_length_default)
+	except:
+		errorWith.append('keylength')
+		key_length = key_length_default
+	if not key_length in valid_key_lengths:
+		key_length = key_length_default
+		errorWith.append('keylength')
+	
+	try:
+		debug = config_client.getboolean('debug', debug_default)
+	except:
+		errorWith.append('debug')
+		debug = debug_default
+	
+	print('Errors with loading [{}] from config file.'.format(', '.join(errorWith)) * bool(len(errorWith)))
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='Connect to a dedicated server.\nAll settings here override the config file.')
+	parser.add_argument('-p', '--port', type=int, help='Specify the port number to connect to.', action='store')
+	parser.add_argument('-sA', '--serveraddress', type=str, help='Specify the server address to connect to.', action='store')
+	parser.add_argument('-kL', '--keylength', type=int, help='Specify the AES key length.', action='store')
+	parser.add_argument('-nE', '--noencryption', help='Specify this to disable encryption.', action='store_true')
+	parser.add_argument('-pK', '--publickey', type=str, help='Specify the public key to use when connecting to the server.', action='store')
+	parser.add_argument('-c', '--config', type=str, help='Specify the config file to use.', action='store')
+	parser.add_argument('-d', '--debug', help=argparse.SUPPRESS, action='store_true')
+	args = parser.parse_args()
+	global publicKeyFile, port, do_encrypt, debug, server_address, key_length, config_file
+	if args.config:
+		config_file = args.config
+		parse_config()
+	if args.publickey:
+		publicKeyFile = args.publickey
+	if args.noencryption:
+		do_encrypt = not args.noencryption
+	if args.port:
+		port = args.port
+	if args.serveraddress:
+		server_address = args.serveraddress
+	if args.keylength:
+		key_length = args.keylength
+	if args.debug:
+		debug = args.debug
 
 def decryptRSA(encrypted):
 	decryptor = PKCS1_OAEP.new(privateKey)
@@ -33,8 +124,11 @@ def encryptAES(decrypted, key):
 
 def sendToServer(message):
 	message = message.encode('utf-8')
-	encMessage = encryptAES(message, key)
-	s.send(encMessage)
+	if do_encrypt:
+		encMessage = encryptAES(message, key)
+		s.send(encMessage)
+	else:
+		s.send(message)
 
 def clear():
 	if os.name == 'nt':
@@ -42,61 +136,95 @@ def clear():
 	else:
 		_ = os.system('clear')
 
+def numberOfLines(message):
+	return (len(message) // shutil.get_terminal_size().columns) + 1
+
 def listen():
 	while True:
 		try:
-			inMessageEnc = s.recv(1024)
-			inMessage = decryptAES(inMessageEnc, key).decode('utf-8')
-		except:
-			break
+			if do_encrypt:
+				inMessageEnc = s.recv(1024)
+				inMessage = decryptAES(inMessageEnc, key).decode('utf-8')
+				if inMessage == b'Encryption is not allowed here.':
+					print("Encryption is not allowed here, sorry.")
+					raise
+			else:
+				inMessage = s.recv(1024).decode('utf-8')
+		except Exception as err:
+			if debug:
+				print(err)
+			s.close()
+			exit()
 		print('\r\x1b[2K{}'.format(inMessage))
 		print("{}> ".format(name), end="", flush=True)
 
-print("CLIENT")
+if __name__ == "__main__":
+	print("CLIENT")
+	
+	config_file = 'client.ini'
+	parse_config()
+	parse_args()
+	
+	if debug:
+		print("DEBUG")
 
-#if not os.path.exists('public.pem'):
-#	print("Downloading public RSA key.")
-#	urllib.request.urlretrieve("http://web.server/public.pem", "public.pem")
+	#if not os.path.exists(publicKeyFile):
+	#	print("Downloading public RSA key.")
+	#	urllib.request.urlretrieve("http://{}/{}".format(server_address, publicKeyFile), publicKeyFile)
 
-with open("public.pem", "r") as file:
-	publicKeyString = file.read()
-publicKey = RSA.import_key(publicKeyString)
+	if do_encrypt:
+		with open(publicKeyFile, "r") as file:
+			publicKeyString = file.read()
+		publicKey = RSA.import_key(publicKeyString)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-colorama.init()
-erase = '\x1b[1A\x1b[2K'
-name = input("Your username: ")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	colorama.init()
+	erase = '\x1b[1A\x1b[2K'
+	name = input("Your username: ")
 
-try:
-	print("Connecting...\r", end="")
-	#s.connect(("web.server",5006))
-	s.connect(("127.0.0.1",5006))
-	key = os.urandom(16)
-	keyEnc = encryptRSA(key)
-	s.send(keyEnc)
-	print("Connected with AES.")
-	initialResponseEnc = s.recv(1024)
-	initialResponse = decryptAES(initialResponseEnc, key)
-	if initialResponse == b'Who are you?':
+	try:
+		print("Connecting...\r", end="")
+		s.connect((server_address, port))
+		if do_encrypt:
+			key = os.urandom(key_length)
+			keyEnc = encryptRSA(key)
+			s.send(keyEnc)
+			print("Connected with AES.")
+		
 		sendToServer(name)
-		connectedEnc = s.recv(1024).decode('utf-8')
-		connected = decryptAES(connectedEnc, key).decode('utf-8')
+		if do_encrypt:
+			connectedEnc = s.recv(1024).decode('utf-8')
+			connected = decryptAES(connectedEnc, key).decode('utf-8')
+		else:
+			connected = s.recv(1024).decode('utf-8')
 		print(connected)
-	Thread(target=listen).start()
-	while True:
-		outMessage = input("{}> ".format(name))
-		if outMessage == "exit" or outMessage == "quit":
-			break
-		outMessageCombo = "{}> {}".format(name, outMessage)
-		print((erase * ((len(outMessageCombo) // shutil.get_terminal_size().columns) + 1)) + outMessageCombo)
-		if outMessage == "cls" or outMessage == "clear":
-			clear()
-			continue
-		sendToServer(outMessage)
-except Exception as err:
-	print("Error Connecting")
-	print(err)
-finally:
-	sendToServer('I am out.')
-	s.close()
-	print("Closed Socket")
+		
+		Thread(target=listen).start()
+		while True:
+			try:
+				outMessage = input("{}> ".format(name))
+			except KeyboardInterrupt:
+				print("\nOkay, bye.")
+				s.close()
+				exit()
+			if outMessage == "exit" or outMessage == "quit":
+				break
+			outMessageCombo = "{}> {}".format(name, outMessage)
+			print((erase * numberOfLines(outMessageCombo)) + outMessageCombo)
+			if outMessage == "cls" or outMessage == "clear":
+				clear()
+				continue
+			sendToServer(outMessage)
+	except Exception as err:
+		print("Cannot Connect To Server")
+		print("Check Configuration and Try Again")
+		if debug:
+			print(err)
+	finally:
+		try:
+			sendToServer('I am out.')
+		except Exception as err:
+			if debug:
+				print(err)
+		s.close()
+		print("Connection Closed With Server")
--- Server.py
+++ Server.py
@@ -9,6 +9,86 @@ from Cryptodome.PublicKey import RSA
 from Cryptodome.Cipher import PKCS1_OAEP
 import ast
 from AESCipher import AESCipher
+import configparser
+import argparse
+
+def parse_config():
+	global privateKeyFile, port, max_users, debug, allow_encryption
+	privateKeyFile_default = 'private.pem'
+	port_default = 5006
+	max_users_default = 2048
+	debug_default = False
+	config_file_default = 'server.ini'
+	allow_encryption_default = True
+	
+	config = configparser.ConfigParser()
+	config.read(config_file)
+	section = "Server"
+	try:
+		config_server = config[section]
+	except:
+		privateKeyFile = privateKeyFile_default
+		port = port_default
+		max_users = max_users_default
+		debug = debug_default
+		allow_encryption = allow_encryption_default
+		if config_file != config_file_default:
+			print("\nSomething wrong with config file ({}).\n".format(config_file))
+		return
+	
+	errorWith = []
+	
+	privateKeyFile = config_server.get('privatekeyfile', privateKeyFile_default)
+	
+	try:
+		port = config_server.getint('port', port_default)
+	except:
+		errorWith.append('port')
+		port = port_default
+	
+	try:
+		max_users = config_server.getint('maxusers', max_users_default)
+	except:
+		errorWith.append('maxusers')
+		max_users = max_users_default
+	
+	try:
+		allow_encryption = config_server.getboolean('allowencryption', debug_default)
+	except:
+		errorWith.append('allowencryption')
+		allow_encryption = allow_encryption_default
+	
+	try:
+		debug = config_server.getboolean('debug', debug_default)
+	except:
+		errorWith.append('debug')
+		debug = debug_default
+	
+	if errorWith: print('\nErrors with loading [{}] from config file.\n'.format(', '.join(errorWith)))
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='Provides a dedicated server for Clients to connect to.\nAll settings here override the config file.')
+	parser.add_argument('-p', '--port', type=int, help='Specify the port number to host on.', action='store')
+	parser.add_argument('-m', '--maxusers', type=int, help='Specify the max number of users that can connect.', action='store')
+	parser.add_argument('-pK', '--privatekey', help='Specify the private key to use.', action='store')
+	parser.add_argument('-c', '--config', help='Specify the config file to use.', action='store')
+	parser.add_argument('-dE', '--dontencrypt', help="Specify if clients shouldn't be able to use encryption when connecting.", action='store_true')
+	parser.add_argument('-d', '--debug', help=argparse.SUPPRESS, action='store_true')
+	args = parser.parse_args()
+	global config_file, privateKeyFile, port, max_users, allow_encryption, debug
+	if args.config:
+		config_file = args.config
+		parse_config()
+	if args.privatekey:
+		privateKeyFile = args.privatekey
+	if args.maxusers:
+		max_users = args.maxusers
+	if args.port:
+		port = args.port
+	if args.dontencrypt:
+		allow_encryption = not args.dontencrypt
+	if args.debug:
+		debug = args.debug
 
 def decryptRSA(encrypted):
 	decryptor = PKCS1_OAEP.new(privateKey)
@@ -33,50 +113,80 @@ def encryptAES(decrypted, key):
 def broadcast(message, senderID):
 	for eachID, eachConn in connections.items():
 		if eachID != senderID:
-			eachConn.send(encryptAES(message, keys[eachID]))
+			if keys.get(eachID):
+				eachConn.send(encryptAES(message, keys[eachID]))
+			else:
+				eachConn.send(message)
 
 def hereList():
 	return "Here exists {}.".format(', '.join(list(names.values())))
 
 def getID():
-	rand = random.randint(0,2047)
+	rand = random.randint(0, max_users - 1)
 	while rand in list(names.keys()):
-		rand = random.randint(0,2047)
+		rand = random.randint(0, max_users - 1)
 	return rand
 
 def talk(conn, addr):
 	print("Opening connection with {}.".format(addr))
+	
 	try:
 		rand = getID()
 		ID = str(rand)
 		connections[ID] = conn
-		keyEnc = conn.recv(1024)
+		initialMessage = conn.recv(1024)
 		try:
-			key = decryptRSA(keyEnc)
+			key = decryptRSA(initialMessage)
+			keys[ID] = key
+			nameEnc = conn.recv(1024)
+			name = decryptAES(nameEnc, key).decode('utf-8')
+			if not allow_encryption:
+				conn.send(encryptAES(b'Encryption is not allowed here.', key))
+				return
 		except ValueError as err:
+			try:
+				name = initialMessage.decode('utf-8')
+			except Exception as err:
+				print("Malformed input, kicking.")
+				conn.send(b'Malformed input, kicking.')
+				if debug:
+					print(err)
+				return
+		except Exception as err:
 			print("Malformed input, kicking.")
+			conn.send(b'Malformed input, kicking.')
+			if debug:
+				print(err)
 			return
-		keys[ID] = key
-		conn.send(encryptAES(b'Who are you?', key))
-		nameEnc = conn.recv(1024)
-		name = decryptAES(nameEnc, key).decode('utf-8')
+
 		names[ID] = name
+		
 		print("They are {} with the ID of {}.".format(name, ID))
-		conn.send(encryptAES(hereList().encode('utf-8'), key))
+		if keys.get(ID):
+			conn.send(encryptAES(hereList().encode('utf-8'), key))
+		else:
+			conn.send(hereList().encode('utf-8'))
+		
 		broadcast("{} has joined.".format(name).encode('utf-8'), ID)
 		while True:
 			try:
-				dataEnc = conn.recv(1024)
-				data = decryptAES(dataEnc, key)
-			except KeyboardInterrupt as err:
-				break
+				if keys.get(ID):
+					dataEnc = conn.recv(1024)
+					data = decryptAES(dataEnc, key)
+				else:
+					data = conn.recv(1024)
 			except Exception as err:
 				print("Error Receiving")
 				data = b'exit'
+				if debug:
+					print(err)
 			if data == b'I am out.' or data == b'exit' or data == b'quit':
 				break
 			elif data == b'who':
-				conn.send(encryptAES(hereList().encode('utf-8'), key))
+				if keys.get(ID):
+					conn.send(encryptAES(hereList().encode('utf-8'), key))
+				else:
+					conn.send(hereList().encode('utf-8'))
 			elif data:
 				message = "{}> {}".format(name, data.decode('utf-8'))
 				message = message.encode('utf-8')
@@ -87,31 +197,58 @@ def talk(conn, addr):
 				broadcast(message, ID)
 	finally:
 		try:
-			del connections[ID]
-			del keys[ID]
-			del names[ID]
+			if connections.get(ID):
+				del connections[ID]
+			if keys.get(ID):
+				del keys[ID]
+			if names.get(ID):
+				del names[ID]
 			print("Closed connection with {}.".format(addr))
 			print("They were {} with the ID of {}.".format(name, ID))
 			message = "{} has left.".format(name).encode('utf-8')
 			broadcast(message, ID)
-		except:
+		except Exception as err:
 			print("Some kind of error in cleaning up that connection. They probably didn't have a name or key.")
+			if debug:
+				print(err)
 		conn.close()
 
-print("SERVER")
+def listening():
+	while True:
+		conn, addr = s.accept()
+		Thread(target=talk, args=(conn, addr)).start()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("0.0.0.0",5006))
-s.listen(5)
-
-with open("private.pem", "r") as file:
-	privateKeyString = file.read()
-privateKey = RSA.import_key(privateKeyString)
-
-connections = {}
-names = {}
-keys = {}
-
-while True:
-	conn, addr = s.accept()
-	Thread(target=talk, args=(conn, addr)).start()
+if __name__ == "__main__":
+	print("SERVER")
+	
+	config_file = "server.ini"
+	parse_config()
+	parse_args()
+	
+	if debug:
+		print("DEBUG")
+		#exit()
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(("0.0.0.0",port))
+	s.listen(5)
+	
+	with open(privateKeyFile, "r") as file:
+		privateKeyString = file.read()
+	privateKey = RSA.import_key(privateKeyString)
+	
+	connections = {}
+	names = {}
+	keys = {}
+	
+	thread = Thread(target=listening)
+	thread.daemon = True
+	thread.start()
+	
+	try:
+		while True:
+			time.sleep(100)
+	except KeyboardInterrupt:
+		s.close()
+		print("Closing Server")
+		exit()
